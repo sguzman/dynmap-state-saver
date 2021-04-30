872f1a7009c0219bf1865f9196127b063ca2b4ff
Salvador Guzman <guzmansalv@gmail.com>
2021-04-30 13:02:25 -0700

    code looks good but need to debug why session isnt updating

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import json
 import logging
 import requests
+import time
 from typing import Dict
 
-url: str = 'https://emperor.hexa-network.net'
+
+request_session = requests.Session()
+
 
 def init_log() -> None:
     logging.basicConfig(
@@ -15,16 +18,33 @@ def init_log() -> None:
 def init() -> None:
     init_log()
 
-def config() -> Dict:
-    config_url: str = f'{url}/up/configuration'
-    resp: str = requests.get(config_url).text
-    obj: Dict = json.loads(resp)
+def get(path: str) -> str:
+    full_url: str = f'https://emperor.hexa-network.net/up/{path}'
+    return request_session.get(full_url).text
 
+def get_json(path: str) -> Dict:
+    resp: str = get(path)
+    return json.loads(resp)
+
+
+def config() -> Dict:
+    return get_json('configuration')
+
+def milli() -> int:
+    return int(time.time() * 10000)
+
+def update() -> Dict:
+    return get_json(f'world/world/{milli()}')
+
+def pretty(obj: Dict) -> str:
     return json.dumps(obj, indent=4, sort_keys=True)
 
 def main() -> None:
     init()
-    print(config())
+    
+    while True:
+        logging.info(pretty(update()))
+        time.sleep(2)    
 
 if __name__ == "__main__":
     main()
