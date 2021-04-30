44ce0578a2818a16de44f18105793e71220143bb
Salvador Guzman <guzmansalv@gmail.com>
2021-04-30 12:48:07 -0700

    pretty print

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
+import json
 import logging
+import requests
+from typing import Dict
+
+url: str = 'https://emperor.hexa-network.net'
 
 def init_log() -> None:
     logging.basicConfig(
@@ -10,8 +15,16 @@ def init_log() -> None:
 def init() -> None:
     init_log()
 
+def config() -> Dict:
+    config_url: str = f'{url}/up/configuration'
+    resp: str = requests.get(config_url).text
+    obj: Dict = json.loads(resp)
+
+    return json.dumps(obj, indent=4, sort_keys=True)
+
 def main() -> None:
     init()
+    print(config())
 
 if __name__ == "__main__":
     main()
